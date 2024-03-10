052a012450ce5da4ef4eeb59dceb30c39f8844dc
Erin A <erinaviles83@gmail.com>
2024-03-09 22:00:55 -0500

    initial commit/redone

---
+++ Dictionary.js
@@ -0,0 +1,33 @@
+import React, { useState } from "react";
+import axios from "axios";
+import "./Dictionary.css"; 
+
+export default function Dictionary() {
+    let [keyword, setKeyword] = useState("");
+
+    function handleResponse(response) {
+        console.log(response.data[0]); 
+    }
+
+    function handleKeywordChange(event) {
+        setKeyword(event.target.value); 
+    }
+
+    function search(event) {
+        event.preventDefault(); 
+  
+
+    let apiUrl = `https://api.shecodes.io/dictionary/v1/define?word=${keyword}&key={key}`; 
+    
+    axios.get(apiUrl).then(handleResponse); 
+    }
+
+    return (
+        <div className="Dictionary">
+            <form onSubmit={search}>
+                <input type="search" onChange={handleKeywordChange}/>
+            </form>
+        </div>
+    ); 
+
+}
